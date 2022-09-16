4979df66d87282a881b15307caca37575d455024
Lennard Vanderspek <lennardvanderspek@gmail.com>
2022-09-15 22:17:54 -0700

    Adds memory map as array

--- src/cpu.rs
+++ src/cpu.rs
@@ -4,6 +4,7 @@ pub struct CPU {
     pub register_y: u8,
     pub status: u8,
     pub program_counter: u16,
+    memory: [u8; 0xffff],
 }
 
 impl CPU {
@@ -14,7 +15,48 @@ impl CPU {
             register_y: 0,
             status: 0,
             program_counter: 0,
+            memory: [0; 0xffff],
         }
+    }
+
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos + 1) as u16;
+        (hi << 8) | (lo as u16)
+    }
+
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos + 1, hi);
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.register_y = 0;
+        self.program_counter = self.mem_read_u16(0xfffc);
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.program_counter = 0x8000;
+        self.mem_write_u16(0xfffc, 0x8000);
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run();
     }
 
     fn update_processor_status(&mut self, condition: bool, flag: u8) {
@@ -137,10 +179,9 @@ impl CPU {
     fn _txs() {}
     fn _tya() {}
 
-    pub fn run(&mut self, program: Vec<u8>) {
-        self.program_counter = 0;
+    pub fn run(&mut self) {
         loop {
-            let opcode = program[self.program_counter as usize];
+            let opcode = self.mem_read(self.program_counter);
             self.program_counter += 1;
 
             match opcode {
@@ -148,7 +189,7 @@ impl CPU {
                     return;
                 },
                 0xa9 => { // LDA
-                    let param = program[self.program_counter as usize];
+                    let param = self.mem_read(self.program_counter);
                     self.program_counter += 1;
                     self.lda(param);
                 },
--- tests/test_cpu.rs
+++ tests/test_cpu.rs
@@ -8,7 +8,7 @@ mod test {
     #[test]
     fn test_0xaa_tax_transfer_num() {
         let mut cpu = CPU::new();
-        cpu.run(vec![0xa9, 0x05, 0xaa, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x05, 0xaa, 0x00]);
         assert_eq!(cpu.register_x, 0x05);
         assert!(cpu.status & 0b0000_0010 == 0);
         assert!(cpu.status & 0b1000_0000 == 0);
@@ -17,21 +17,21 @@ mod test {
     #[test]
     fn test_0xaa_tax_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.run(vec![0xaa, 0x00]);
+        cpu.load_and_run(vec![0xaa, 0x00]);
         assert!(cpu.status & 0b0000_0010 == 0b10);
     }
 
     #[test]
     fn test_0xaa_tax_negative_flag() {
         let mut cpu = CPU::new();
-        cpu.run(vec![0xa9, 0x80, 0xaa, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x80, 0xaa, 0x00]);
         assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
     }
 
     #[test]
     fn test_0xa9_lda_imidiate_load_data() {
         let mut cpu = CPU::new();
-        cpu.run(vec![0xa9, 0x05, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
         assert_eq!(cpu.register_a, 0x05);
         assert!(cpu.status & 0b0000_0010 == 0);
         assert!(cpu.status & 0b1000_0000 == 0);
@@ -40,14 +40,14 @@ mod test {
     #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.run(vec![0xa9, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
         assert!(cpu.status & 0b0000_0010 == 0b10);
     }
 
     #[test]
     fn test_0xa9_lda_negative_flag() {
         let mut cpu = CPU::new();
-        cpu.run(vec![0xa9, 0x80, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x80, 0x00]);
         assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
     }
 }
