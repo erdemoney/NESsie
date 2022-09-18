71df70ca8e6633fa5661f8c1fcc4ed4b6a460d82
Eric DeMoney <eric@demoney.net>
2022-09-17 17:46:56 -0700

    Moves integration tests to unit tests

--- src/cpu.rs
+++ src/cpu.rs
@@ -266,3 +266,60 @@ impl CPU {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_0xaa_tax_transfer_num() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0x05, 0xaa, 0x00]);
+        assert_eq!(cpu.register_a, 0x05);
+        assert_eq!(cpu.register_x, 0x05);
+        assert!(cpu.status & 0b0000_0010 == 0);
+        assert!(cpu.status & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    fn test_0xaa_tax_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xaa, 0x00]);
+        assert_eq!(cpu.register_a, 0x00);
+        assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
+
+    #[test]
+    fn test_0xaa_tax_negative_flag() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0x80, 0xaa, 0x00]);
+        assert_eq!(cpu.register_a, 0x80);
+        assert_eq!(cpu.register_x, 0x80);
+        assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
+    }
+
+    #[test]
+    fn test_0xa9_lda_imidiate_load_data() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
+        assert_eq!(cpu.register_a, 0x05);
+        assert!(cpu.status & 0b0000_0010 == 0);
+        assert!(cpu.status & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
+        assert!(cpu.register_a == 0);
+        assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
+
+    #[test]
+    fn test_0xa9_lda_negative_flag() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0x80, 0x00]);
+        assert_eq!(cpu.register_a, 0x80);
+        assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
+    }
+}
